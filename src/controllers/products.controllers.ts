f5747df3770d0653df93b50a6409963632c228ea
Johny Villegas <johny.villegas.dev@gmail.com>
2026-03-13 19:21:41 +0100

    se agrega sale_type

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -15,15 +15,14 @@ export const getProducts = async (req: Request, res: Response) => {
 
 // Crear producto
 export const createProduct = async (req: Request, res: Response) => {
-  const { name, barcode, price, vat, category_id } = req.body;
+  const { name, barcode, price, vat, category_id, sale_type } = req.body;
   try {
-
     // crear el producto en la base de datos
     const result = await pool.query(
-      `INSERT INTO products (name, barcode, price, vat, category_id, active)
-       VALUES ($1,$2,$3,$4,$5,true)
+      `INSERT INTO products (name, barcode, price, vat, category_id, sale_type, active)
+       VALUES ($1,$2,$3,$4,$5,$6,true)
        RETURNING *`,
-      [name, barcode, price, vat, category_id],
+      [name, barcode, price, vat, category_id, sale_type],
     );
 
     // agregar el producto al inventario con cantidad 0
@@ -42,12 +41,12 @@ export const createProduct = async (req: Request, res: Response) => {
 // Editar producto
 export const updateProduct = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name, barcode, price, vat, category_id } = req.body;
+  const { name, barcode, price, vat, category_id, sale_type } = req.body;
   try {
     const result = await pool.query(
-      `UPDATE products SET name=$1, barcode=$2, price=$3, vat=$4, category_id=$5
-       WHERE id=$6 RETURNING *`,
-      [name, barcode, price, vat, category_id, id],
+      `UPDATE products SET name=$1, barcode=$2, price=$3, vat=$4, category_id=$5, sale_type=$6
+       WHERE id=$7 RETURNING *`,
+      [name, barcode, price, vat, category_id, sale_type, id],
     );
     if (result.rows.length === 0) {
       return res.status(404).json({ response: "error", message: "Product not found" });
@@ -94,7 +93,7 @@ export const getProductByBarcode = async (req: Request, res: Response) => {
   const { barcode } = req.params
   try {
     const result = await pool.query(
-      `SELECT p.id, p.name, p.barcode, p.price, p.vat, i.quantity AS stock
+      `SELECT p.id, p.name, p.barcode, p.price, p.vat, p.sale_type, i.quantity AS stock
         FROM products p
         JOIN inventory i ON p.id = i.product_id
         WHERE p.barcode = $1 AND p.active = true;
