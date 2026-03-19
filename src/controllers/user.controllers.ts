8db70298c64e3f9b45b723bb3afdc3808912527b
Johny Villegas <johny.villegas.dev@gmail.com>
2026-03-19 17:25:08 +0100

    serviciop de traer los usuarios

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -26,3 +26,23 @@ export const register = async (req: Request, res: Response) => {
     res.status(500).json({ response: "error", message: err.message });
   }
 };
+
+export const getUsers = async (req: Request, res: Response) => {
+  const { store_id } = req.params;
+  console.log(store_id);
+  try {
+    const result = await pool.query(
+      `SELECT * FROM public.users
+        WHERE active = true
+        AND ($1::uuid IS NULL OR store_id = $1);`,
+      [store_id]);
+    console.log(result);
+    res.status(200).json({
+      response: "success",
+      users: result.rows,
+    });
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).json({ response: "error", message: err.message });
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/user.controllers";
+import { getUsers, register } from "../controllers/user.controllers";
 
 const router = Router();
 
 router.post("/register", register); // registrar usuario/cajero
+router.get("/:store_id", getUsers); // obtener usuarios
 
 export default router;
