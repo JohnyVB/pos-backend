a1badfb28ed11a09acb3349ce5983fe0131214f6
Johny Villegas <johny.villegas.dev@gmail.com>
2026-03-24 22:53:27 +0100

    ajustes en las rutas de usuario

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUsers, register, toggleUserStatus } from "../controllers/user.controllers";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.post("/register", register); // registrar usuario/cajero
-router.get("/:store_id", getUsers); // obtener usuarios
-router.patch("/toggle-status/:user_id", toggleUserStatus); // activar/desactivar usuario
+router.post("/register", authMiddleware, register); // registrar usuario/cajero
+router.get("/:store_id", authMiddleware, getUsers); // obtener usuarios
+router.patch("/toggle-status/:user_id", authMiddleware, toggleUserStatus); // activar/desactivar usuario
 
 export default router;
